891e9a69e7bf054c05e4685c14a53e998de6f9d6
Muhammad Ali Ahsan <WiseAliWise@gmail.com>
2025-02-27 21:24:26 +0500

    Initial commit for testing MapBox API from flutter

---
+++ mapbox_testing/android/app/src/main/kotlin/com/example/mapbox_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mapbox_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mapbox_testing/lib/token.dart
@@ -0,0 +1,3 @@
+// MapBox Token
+const String mapBoxToken =
+    "pk.eyJ1IjoiZWFnZXJwayIsImEiOiJjbTduampmcDEwMW0wMmtzMno1aTBoa2JrIn0.ChS7s_tBEgceQ_pJ5MoCOA";
